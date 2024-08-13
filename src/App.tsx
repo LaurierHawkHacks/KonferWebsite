8a00f38f3d4fe3e186fe75d473dc675eaf3640b7
Nausher Rao <sherrao15@gmail.com>
2024-08-12 22:20:31 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Konfer</h1>
+      <div className="card">
+        <p>
+          Edit src/App.tsx and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
