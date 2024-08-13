ffb72c9d0ee5d32dec9be51a2a849e72a568cba9
jc <46619361+juancwu@users.noreply.github.com>
2024-08-12 23:08:44 -0400

    add hh,lcs,yacht links + about us

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import './App.css';
 
 const App = () => {
-  return (
-    <div className="container">
-      <h1>Konfer</h1>
-      <div className="card">
-        <p>
-          Edit src/App.tsx and save to test HMR
-        </p>
-      </div>
+    return (
+        <div className="container">
+            <h1>Konfer</h1>
+            <div className="card">
+                <p>
+                    Konfer bridges innovative companies with the next generation
+                    of tech talent. We organize high-impact events connecting
+                    businesses to ambitious students and young professionals.{' '}
+                </p>
+            </div>
+            <div className="link-container">
+                <a href="https://hawkhacks.ca" target="_blank">
+                    HawkHacks
+                </a>
+                <a href="https://lauriercs.ca" target="_blank">
+                    Laurier Computing Society
+                </a>
+                <a href="https://lu.ma/8iwq9lxb" target="_blank">
+                    Luma Yacht Party at Collision
+                </a>
+            </div>
+        </div>
+    );
+};
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
+export default App;
